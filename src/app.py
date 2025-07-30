ffee2e2f4a6aad17cedcfe3697b4595a69acb4b3
Ana Coelho <70607969+anabcoelho@users.noreply.github.com>
2025-07-30 16:15:35 -0300

    update

--- src/app.py
+++ src/app.py
@@ -1,60 +1,15 @@
-import os
-import time
+from pytube import YouTube
 import streamlit as st
-import yt_dlp
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ffmpeg_path = os.path.join(BASE_DIR, '..', 'ffmpeg', 'bin')
-output_directory = os.path.join(BASE_DIR, 'downloads')
-os.makedirs(BASE_DIR, exist_ok=True)
+st.title("YouTube Downloader (sem servidor!)")
 
-def download_audio(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'ffmpeg_location': ffmpeg_path,
-        'http_headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0'
-        },
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': f'{output_directory}/%(title)s.%(ext)s',
-    }
+url = st.text_input("Cole a URL do YouTube")
 
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(info)
-        mp3_file = os.path.splitext(filename)[0] + ".mp3"
-        return mp3_file
-
-st.title("🎵 YouTube Downloader - Áudio MP3")
-url = st.text_input("Cole a URL do vídeo do YouTube")
-
-if st.button("Baixar"):
-    if url:
-        with st.spinner("Baixando e convertendo..."):
-            try:
-                mp3_path = download_audio(url)
-                with open(mp3_path, "rb") as f:
-                    st.success("✅ Download concluído!")
-                    st.download_button(
-                        label="📥 Baixar MP3",
-                        data=f.read(),
-                        file_name=os.path.basename(mp3_path),
-                        mime="audio/mpeg",
-                        key="download-btn"
-                    )
-
-                # ⚠️ Apagar o arquivo depois de pequeno atraso
-                time.sleep(30)  # Espera garantir que download começou
-                if os.path.exists(mp3_path):
-                    os.remove(mp3_path)
-                    
-
-            except Exception as e:
-                st.error(f"❌ Erro: {e}")
-    else:
-        st.warning("⚠️ Por favor, insira uma URL válida.")
+if st.button("Gerar link de download"):
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.filter(only_audio=True).first()
+        st.success(f"Download direto disponível:")
+        st.markdown(f"[Clique aqui para baixar o áudio]({stream.url})")
+    except Exception as e:
+        st.error(f"Erro: {e}")
